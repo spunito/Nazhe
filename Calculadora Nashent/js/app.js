8033f5c3edeee2f65a191a075c3d786520b47bff
spunito <naclif30@gmail.com>
2022-03-15 18:26:36 -0300

    Se agrega el Proyecto

---
+++ Calculadora Nashent/js/app.js
@@ -0,0 +1,14 @@
+console.log("HOLA MAIGOS DEL YOUTUBE")
+function hola(){
+    console.log("Hola amigos como estás");
+
+}
+window.document.title = 'Calculadora Nashe';
+function suma()
+        {
+    const suma1=document.getElementById('num1').value;
+    const suma2=document.getElementById('num2').value
+    const result=document.getElementById("result").value =suma1+suma2;
+    
+    }
+

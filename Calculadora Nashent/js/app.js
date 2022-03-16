b021eac8fba8f427584962ae961ea29667f6e649
Axel Muñoz <83663289+spunito@users.noreply.github.com>
2022-03-16 18:13:28 -0300

    Update app.js

--- Calculadora Nashent/js/app.js
+++ Calculadora Nashent/js/app.js
@@ -9,6 +9,8 @@ function suma()
     const suma1=document.getElementById('num1').value;
     const suma2=document.getElementById('num2').value
     const result=document.getElementById("result").value =suma1+suma2;
-    
     }
+
+//esta hecho a proposito que sea una concatenacion ,, pero si se le pone un parseInt a las variables este error se resuelve
+//de igual manera si en los input se hace que sea solo para numeros tambien se soluciona el problema
 
